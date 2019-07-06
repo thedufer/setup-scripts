57103bb05abcf2fa418daabd0e0d3adf8b4285be
Aaron Dufour <thedufer@gmail.com>
2019-07-06 13:48:34 +0800

    add some stuff

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt-get install emacs gnome-terminal opam git m4 emacs25
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+opam init
+opam switch install 4.07.0
+opam install dune core async merlin ocp-indent
+git config --global user.email thedufer@gmail.com
+git config --global user.name "Aaron Dufour"
+ssh-keygen -t rsa -b 4096 -C "thedufer@gmail.com"
