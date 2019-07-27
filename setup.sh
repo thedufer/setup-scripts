4a70721ecdd65c63ee1a62e9073455eb6936c40a
Aaron Dufour <thedufer@gmail.com>
2019-07-27 20:31:46 +0800

    update after actually trying the setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-apt-get install gnome-terminal opam git m4 emacs25 inotify-tools
+sudo apt-get install gnome-terminal opam git m4 emacs25 inotify-tools
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 opam init
 opam switch install 4.07.0
-opam install dune core async merlin ocp-indent
+opam install dune core async merlin ocp-indent cohttp-async
 git config --global user.email thedufer@gmail.com
 git config --global user.name "Aaron Dufour"
 ssh-keygen -t rsa -b 4096 -C "thedufer@gmail.com"
