dd4c136dc2c25e02fe58bf1b15bfaaf3d7985f10
Aaron Dufour <thedufer@gmail.com>
2019-08-29 22:24:30 +0800

    add js_of_ocaml-compiler

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo apt-get install gnome-terminal opam git m4 emacs25 inotify-tools
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 opam init
 opam switch install 4.07.0
-opam install dune core async merlin ocp-indent cohttp-async
+opam install dune core async merlin ocp-indent cohttp-async js_of_ocaml-compiler
 git config --global user.email thedufer@gmail.com
 git config --global user.name "Aaron Dufour"
 ssh-keygen -t rsa -b 4096 -C "thedufer@gmail.com"
